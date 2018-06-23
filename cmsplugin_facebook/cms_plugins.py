ae8b47915f1a1582dea2ea3de5d5529f4596e358
Richard Barran <richard@arbee-design.co.uk>
2018-06-23 08:52:52 +0200

    Plugin editors broken in Django 3.4.5.

--- cmsplugin_facebook/cms_plugins.py
+++ cmsplugin_facebook/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cmsplugin_facebook import models
 
+
 class BasePlugin(CMSPluginBase):
     name = None
 
@@ -12,6 +13,7 @@ class BasePlugin(CMSPluginBase):
                                context['request'].build_absolute_uri()})
         return context
 
+
 class FacebookLikeBoxPlugin(BasePlugin):
     model = models.FacebookLikeBox
     name = 'Facebook Like Box'
@@ -19,12 +21,14 @@ class FacebookLikeBoxPlugin(BasePlugin):
     render_template = 'cmsplugin_facebook/likebox.html'
     change_form_template = 'cmsplugin_facebook/likebox_change_form.html'
 
+
 class FacebookLikeButtonPlugin(BasePlugin):
     model = models.FacebookLikeButton
     name = 'Facebook Like Button'
     module = 'Facebook'
     render_template = 'cmsplugin_facebook/likebutton.html'
     change_form_template = 'cmsplugin_facebook/likebutton_change_form.html'
+
 
 plugin_pool.register_plugin(FacebookLikeBoxPlugin)
 plugin_pool.register_plugin(FacebookLikeButtonPlugin)
